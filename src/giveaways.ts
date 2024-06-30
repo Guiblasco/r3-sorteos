031dbc122660416f24b46b2603b04556425af9b5
Guillem Blasco <gowet92@gmail.com>
2024-06-30 21:48:28 +0200

    Add function entergiveaway

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -1,26 +1,22 @@
 import { askUser } from "./askUser.js";
 import { programData, saveData } from "./storage.js";
 
-import { Giveaway } from "./types.js";
+import { Giveaway, User } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
 
-function exitProcess(exitCode: number): void {
-  process.exit(exitCode);
-}
-export function loginUser(email: string, password: string) {
+export function loginUser(email: string, password: string): void {
   const user = programData.users.find(
-    (user) => user.email == email && user.password == password
+    (user) => user.email === email && user.password === password
   );
-
   if (user) {
     programData.userEmail = email;
-    if (user.email == "admin@admin.com") {
+    if (user.email === "admin@admin.com") {
       programData.isAdmin = true;
     }
     saveData();
   } else {
     console.log("El usuario no coincide con los usuarios registrados");
-    exitProcess(0);
+    process.exit(0);
   }
 }
 export function createGiveaway() {
@@ -70,5 +66,42 @@ export function deleteGiveaway(giveawayNumber: number) {
     programData.giveaways.splice(giveawayNumber - 1, 1);
     console.log(`Sorteo ${giveawayNumber} eliminado con éxito`);
     saveData();
+  }
+}
+
+export function enterGiveaway(enterGiveawayNumber: number) {
+  if (enterGiveawayNumber > programData.giveaways.length) {
+    console.log("No existe este sorteo");
+  } else {
+    const user = programData.users.find((user) => programData.userEmail);
+    if (user) {
+      programData.giveaways[enterGiveawayNumber].participants.push(user);
+    }
+    saveData();
+  }
+}
+
+export function listUserGiveaways() {
+  const arrayGiveaways: Giveaway[] = [];
+  for (const giveaway of programData.giveaways) {
+    for (const participant of giveaway.participants) {
+      if (participant.email == programData.userEmail) {
+        arrayGiveaways.push(giveaway);
+      }
+    }
+  }
+
+  console.log(
+    `Estás inscrito en los siguientes ${arrayGiveaways.length} sorteos`
+  );
+  for (let giveaway = 0; giveaway < arrayGiveaways.length; giveaway++) {
+    console.log(
+      giveaway +
+        1 +
+        ". Sorteo de un " +
+        programData.giveaways[giveaway].name +
+        " en " +
+        programData.giveaways[giveaway].socialNetwork
+    );
   }
 }
